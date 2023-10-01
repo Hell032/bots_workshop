ee22bdeee06c28fcd761422ed3fc75f4c0f761e1
Hell032 <46672252+Hell032@users.noreply.github.com>
2023-10-01 21:17:00 +0200

    changed docker setup

--- setup.sh
+++ setup.sh
@@ -4,10 +4,36 @@
 apt update
 apt upgrade -y 
 
+#install curl and install docker repo via dockers apt repo
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+#install docker stuff
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+#permissions of docker (if necessary!)
+sudo groupadd docker
+sudo usermod -aG docker ${USER}
+
+sudo reboot
+
+#old version
 #install curl and install docker via convenience script
-apt install curl
-curl -fsSL https://get.docker.com -o get-docker.sh
-sh get-docker.sh
+#apt install curl
+#curl -fsSL https://get.docker.com -o get-docker.sh
+#sh get-docker.sh
+
 
 #install splunk enterprise -> change password for accessing the web gui
 #password must be at least 8 chars otherwise splunk throws an error

3756cb8a5d63ea09745392aca95f0da73baea35d
Hell032 <46672252+Hell032@users.noreply.github.com>
2023-11-13 23:41:44 +0100

    Create install_dataset.sh

---
+++ install_dataset.sh
@@ -0,0 +1,12 @@
+echo -e "\n\nexecute in container shell"
+sudo -i
+
+cd /opt/splunk/etc/apps
+
+echo -e "\n\ndownload the bots dataset"
+wget https://botsdataset.s3.amazonaws.com/botsv3/botsv3_data_set.tgz
+
+echo -e "\n\nuntar the file" 
+tar -xvf botsv3_data_set.tgz 
+
+echo -e "\n\nexit the shell and restart docker container"
